d87a6dc12e54859b6ab90583e2f05896e88a8a52
jun <aaasayok@gmail.com>
2019-04-21 21:21:32 +0800

    fix

--- config/payers.php
+++ config/payers.php
@@ -4,6 +4,8 @@ return [
 
     'alipay' => [
 
+        'endpoint' => 'https://mapi.alipay.com/gateway.do',
+
         //合作身份者ID，签约账号，以2088开头由16位纯数字组成的字符串
         'partner' => '########################',
 
